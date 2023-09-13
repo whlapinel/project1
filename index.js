29da1014df9f8da3be7b90901318a7cc5741a89f
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-09-13 11:27:06 -0400

    reformatted header template to make more readable.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const headerTemplate = document.createElement('template');
 
-const header = document.querySelector('header')
+headerTemplate.innerHTML = '\
+<h1 class="title">My Website</h1>\
+<nav class="navbar">\
+<ul>\
+<li>\<a href="index.html">Home</a></li>\
+<li><a href="project.html">Project</a></li>\
+<li><a href="contact.html">Contact</a></li>\
+</ul>\
+</nav>';
 
-headerTemplate.innerHTML = '<h1 class="title" > My Website</h1><nav class="navbar"><ul><li><a href="index.html">Home</a>                </li><li><a href="project.html">Project</a></li>                <li><a href="contact.html">Contact</a>                </li></ul>        </nav>';
+const header = document.querySelector('header')
 
 header.appendChild(headerTemplate.content);
