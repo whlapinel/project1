f54b58d920a6f16a7f5e7227626ee5bb3f459cbf
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-09-13 12:21:02 -0400

    trying to figure out navbar template styling

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const headerTemplate = document.createElement('template');
 
 headerTemplate.innerHTML = '\
 <h1 class="title">My Website</h1>\
-<nav class="navbar">\
+<nav>\
 <ul>\
-<li>\<a href="index.html">Home</a></li>\
-<li><a href="project.html">Project</a></li>\
-<li><a href="contact.html">Contact</a></li>\
+<li class="navbar"><a href="index.html">Home</a></li>\
+<li class="navbar"><a href="project.html">Project</a></li>\
+<li class="navbar"><a href="contact.html">Contact</a></li>\
 </ul>\
 </nav>';
 
