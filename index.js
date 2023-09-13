3b99289205f767ccd478603b57ac6e1e91196aa7
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-09-13 08:41:02 -0400

    messed around some more with header template

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const headerTemplate = document.createElement('template');
 
-headerTemplate.innerHTML = '<h1 class="title">My Website</h1><nav class="navbar"><ul><li><a href="index.html">Home</a></li><li><a href="project.html">Project</a></li><li><a href="contact.html">Contact</a></li></ul></nav>';
+const header = document.querySelector('header')
 
-document.body.appendChild(headerTemplate.content);
+headerTemplate.innerHTML = '<h1 class="title" > My Website</h1><nav class="navbar"><ul><li><a href="index.html">Home</a>                </li><li><a href="project.html">Project</a></li>                <li><a href="contact.html">Contact</a>                </li></ul>        </nav>';
+
+header.appendChild(headerTemplate.content);
