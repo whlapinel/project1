06c5db5e19bd6b61bc2c305be702c5167e2e651e
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-09-13 14:35:58 -0400

    added footer, adjusted navbar

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const headerTemplate = document.createElement('template');
 
 headerTemplate.innerHTML = '\
 <h1 class="title">My Website</h1>\
-<nav>\
-<ul>\
-<li class="navbar"><a href="index.html">Home</a></li>\
-<li class="navbar"><a href="project.html">Project</a></li>\
-<li class="navbar"><a href="contact.html">Contact</a></li>\
-</ul>\
+<nav class="navbar">\
+<a href="index.html">Home</a>\
+<a href="project.html">Project</a>\
+<a href="contact.html">Contact</a>\
 </nav>';
 
-const header = document.querySelector('header')
+const header = document.querySelector('header');
 
 header.appendChild(headerTemplate.content);
+
+const footerTemplate = document.createElement('template');
+
+const footer = document.querySelector('footer');
+
+footerTemplate.innerHTML = '<p class="footer">this is my footer</p>';
+
+footer.appendChild(footerTemplate.content);
